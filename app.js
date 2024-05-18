3dfd613bb4801e5ed352a563b46b45338e2ee7d9
owaissikander <154621974+owaissikander@users.noreply.github.com>
2024-05-18 22:13:30 +0500

    Incomplete

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var child_1 = document.getElementById('child_1')
+var paragraph1 = document.getElementById('paragraph')
+
+
+
+
+function sans(){
+    paragraph1.style.fontFamily= 'sans-serif'
+  
+}
+// function sans() {
+//     paragraph1.style.fontFamily = 'sans-serif'
+// }
+
+function serif() {
+    paragraph1.style.fontFamily = 'sans-serif'
+}
+
+function georgia() {
+    paragraph1.style.fontFamily = "Georgia";
+}
+
+
+function bold() {
+    paragraph1.style.fontWeight = "bold";
+
+}
+
+function italic() {
+    paragraph1.style.fontStyle = "italic";
+
+}
+
+function underline() {
+    paragraph1.style.textDecoration = "underline";
+
+}
+
+function left() {
+    paragraph1.style.textAlign = "start";
+
+}
+
+function center() {
+    paragraph1.style.textAlign = "center";
+
+}
+
+function right() {
+    paragraph1.style.textAlign = "end";
+
+}
